ec91592d01137e6240db582f2965ba89bbf978b7
TatsianaSauko <miljanzowa@mail.ru>
2022-06-20 17:37:35 +0300

    12 commit

--- myshop/myshop/settings.py
+++ myshop/myshop/settings.py
@@ -81,13 +81,21 @@ WSGI_APPLICATION = 'myshop.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'shop',
+        'USER': 'postgres',
+        'PASSWORD': 'admin',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
--- myshop/orders/tasks.py
+++ myshop/orders/tasks.py
@@ -2,6 +2,7 @@ from celery import shared_task
 from django.core.mail import send_mail
 from .models import Order
 
+
 @shared_task
 def order_created(order_id):
     """
